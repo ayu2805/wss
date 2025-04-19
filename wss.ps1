ee2f65bfb1016719a0df80b33bac5869498ffd37
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2025-04-20 00:53:27 +0530

    Code optimisation

--- wss.ps1
+++ wss.ps1
@@ -1,61 +1,156 @@
+$ProgressPreference = 'SilentlyContinue'
+
 # Check if running with administrative privileges
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
 if (-not $isAdmin) {
-  Write-Host "Script is not running with administrative privileges." -ForegroundColor Red
-  Write-Host "Please run PowerShell as Administrator." -ForegroundColor Yellow
-  return
+    Write-Host "Script is not running with administrative privileges." -ForegroundColor Red
+    Write-Host "Please run PowerShell as Administrator." -ForegroundColor Yellow
+    return
 }
 
 # Prompt for new computer name
 $newComputerName = Read-Host "Please enter the new computer name (leave empty to do nothing)"
 if (-not [string]::IsNullOrWhiteSpace($newComputerName)) {
-  if ($newComputerName -match '^[a-zA-Z0-9-]+$' -and $newComputerName.Length -le 15) {
-    Rename-Computer -NewName $newComputerName
-    if ($?) {
-      Write-Host "Computer name changed successfully to $newComputerName."
+    if ($newComputerName -match '^[a-zA-Z0-9-]+$' -and $newComputerName.Length -le 15) {
+        Rename-Computer -NewName $newComputerName -Force
+        if ($?) {
+            Write-Host "Computer name changed successfully to $newComputerName." -ForegroundColor Yellow
+        } else {
+            Write-Host "Failed to change computer name." -ForegroundColor Red
+        }
     } else {
-      Write-Host "Failed to change computer name." -ForegroundColor Red
+        Write-Host "Invalid computer name. Please ensure it contains only letters, numbers, and hyphens, and is no longer than 15 characters." -ForegroundColor Red
     }
-  } else {
-    Write-Host "Invalid computer name. Please ensure it contains only letters, numbers, and hyphens, and is no longer than 15 characters." -ForegroundColor Red
-  }
 } else {
-  Write-Host "No new computer name entered. Exiting without changes."
+    Write-Host "No new computer name entered. Exiting without changes." -ForegroundColor Yellow
 }
 
 # Modify registry settings
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize /t REG_DWORD /v Startupdelayinmsec /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v EnableTaskGroups /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v SnapAssist /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v Start_Layout /d 1 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v UseCompactMode /d 1 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v EnableSnapAssistFlyout /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v EnableSnapBar /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v MultiTaskingAltTabFilter /d 3 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v TaskbarAl /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings /t REG_DWORD /v TaskbarEndTask /d 1 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v HideFileExt /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v Hidden /d 1 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v LaunchTo /d 1 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer /t REG_DWORD /v ShowRecent /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer /t REG_DWORD /v ShowFrequent /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer /t REG_DWORD /v ShowCloudFilesInQuickAccess /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v ShowTaskViewButton /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search /t REG_DWORD /v SearchboxTaskbarMode /d 0 /f
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /t REG_DWORD /v EnableTransparency /d 0 /f
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /t REG_DWORD /v PromptOnSecureDesktop /d 0 /f
-sudo config --enable normal
-Start-Process -FilePath "C:\Windows\Resources\Themes\dark.theme" -Wait; taskkill /f /im SystemSettings.exe
-Stop-Process -Name explorer
+$confirmRegistry = Read-Host "Do you want some basic customisation? (y/N)"
+if ($confirmRegistry -match '^(yes|y)$') {
+    $registrySettings = @(
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'EnableSnapAssistFlyout';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'EnableTaskGroups';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'EnableSnapBar';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'HideFileExt';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'Hidden';
+            Value = 1;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'LaunchTo';
+            Value = 1;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'MultiTaskingAltTabFilter';
+            Value = 3;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'ShowTaskViewButton';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'SnapAssist';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'Start_Layout';
+            Value = 1;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'TaskbarAl';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings';
+            Name = 'TaskbarEndTask';
+            Value = 1;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer';
+            Name = 'ShowCloudFilesInQuickAccess';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer';
+            Name = 'ShowFrequent';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer';
+            Name = 'ShowRecent';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize';
+            Name = 'Startupdelayinmsec';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search';
+            Name = 'SearchboxTaskbarMode';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize';
+            Name = 'EnableTransparency';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System';
+            Name = 'PromptOnSecureDesktop';
+            Value = 0;
+            Type = 'DWord'
+        },
+        @{  Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced';
+            Name = 'UseCompactMode';
+            Value = 1;
+            Type = 'DWord'
+        }
+    )
 
-New-Item -ItemType Directory -Path $env:TEMP\wss -Force | Out-Null
+    foreach ($setting in $registrySettings) {
+        Set-ItemProperty -Path $setting.Path -Name $setting.Name -Value $setting.Value -Type $setting.Type -Force
+    }
+    
+    # Apply theme and restart explorer
+    Start-Process -FilePath "C:\Windows\Resources\Themes\dark.theme" -Wait
+    Stop-Process -Name SystemSettings
+    Stop-Process -Name explorer -Force
+}
+
+
+
+# Create temporary directory
+$tempDir = New-Item -ItemType Directory -Path "$env:TEMP\wss" -Force | Out-Null
 
 # Function to create configuration file
 function Create-ConfigFile {
-  $filePath = "$Env:TEMP\Configuration.xml"
-  $config = @"
+    $config = @"
 <Configuration ID="35d3badb-d62d-4bf5-8438-0b3ee0766659">
   <Add OfficeClientEdition="64" Channel="Current">
     <Product ID="O365ProPlusRetail">
@@ -75,90 +170,127 @@ function Create-ConfigFile {
   <RemoveMSI />
 </Configuration>
 "@
-  
-  if (-not (Test-Path -Path $filePath)) {
-    $config | Out-File -FilePath $filePath -Force
-  }
+
+    Set-Content -Path "$Env:TEMP\Configuration.xml" -Value $config
 }
+
+# Define menu options
+$menuOptions = @(
+    @{  Label = "Cloudflare Warp";
+        Url = "https://1111-releases.cloudflareclient.com/win/latest";
+        FileName = "CloudflareWarp.msi";
+        Arguments = "/quiet"
+    },
+    @{  Label = "Firefox";
+        Url = "https://download.mozilla.org/?product=firefox-latest&os=win64";
+        FileName = "FirefoxSetup.exe";
+        Arguments = "/s"
+    },
+    @{  Label = "Google Chrome";
+        Url = "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi";
+        FileName = "GoogleChrome.msi";
+        Arguments = "/quiet"
+    },
+    @{  Label = "Microsoft Office 365";
+        Url = "https://officecdn.microsoft.com/pr/wsus/setup.exe";
+        FileName = "setup.exe";
+        Arguments = "/configure $Env:TEMP\Configuration.xml"
+    },
+    @{  Label = "Telegram";
+        Url = "https://telegram.org/dl/desktop/win64";
+        FileName = "TelegramSetup.exe";
+        Arguments = "/verysilent"
+    },
+    @{  Label = "Visual Studio Code";
+        Url = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64";
+        FileName = "VSCodeSetup.exe";
+        Arguments = "/verysilent /mergetasks=!runcode"
+    },
+    @{  Label = "Chocolatey";
+        Script = { Invoke-RestMethod -Uri https://community.chocolatey.org/install.ps1 | Invoke-Expression }
+    },
+    @{  Label = "Scoop";
+        Script = { Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression }
+    }
+)
 
 # Function to parse menu selection
 function Parse-MenuSelection {
-  param (
-    [string]$selection
-  )
-  
-  $selected = @()
-  $parts = $selection -split ','
-  
-  foreach ($part in $parts) {
-    if ($part -match "^(\d+)-(\d+)$") {
-      $start = [int]$matches[1]
-      $end = [int]$matches[2]
-      $selected += $start..$end
-    } elseif ($part -match "^\d+$") {
-      $selected += [int]$part
+    param (
+        [string]$selection
+    )
+
+    $selected = @()
+    $parts = $selection -split ','
+
+    foreach ($part in $parts) {
+        if ($part -match "^(\d+)-(\d+)$") {
+            $start = [int]$matches[1]
+            $end = [int]$matches[2]
+            $selected += $start..$end
+        } elseif ($part -match "^\d+$") {
+            $selected += [int]$part
+        }
     }
-  }
-  
-  return ($selected | Sort-Object -Unique)
+
+    return ($selected | Sort-Object -Unique)
 }
 
 # Function to display menu options
 function Show-Menu {
-  Clear-Host
-  Write-Host "=== Menu Options ==="
-  Write-Host "0. Quit"
-  Write-Host "1. Install Cloudflare Warp"
-  Write-Host "2. Install Firefox"
-  Write-Host "3. Install Google Chrome"
-  Write-Host "4. Install Microsoft Office 365"
-  Write-Host "5. Install Telegram"
-  Write-Host "6. Install Visual Studio Code"
-  Write-Host "7. Install Chocolatey"
-  Write-Host "8. Install Scoop"
-  Write-Host "`nYou can select multiple options:"
-  Write-Host "- Single numbers (e.g., 1,3,5)"
-  Write-Host "- Ranges (e.g., 1-3)"
-  Write-Host "- Combinations (e.g., 1-3,5)"
+    Clear-Host
+    Write-Host "=== Menu Options ==="
+    Write-Host "0. Quit"
+    for ($i = 0; $i -lt $menuOptions.Count; $i++) {
+        Write-Host "Install $($i + 1). $($menuOptions[$i].Label)"
+    }
+    Write-Host "`nYou can select multiple options:"
+    Write-Host "- Single numbers (e.g., 1,3,5)"
+    Write-Host "- Ranges (e.g., 1-3)"
+    Write-Host "- Combinations (e.g., 1-3,5)"
 }
 
 # Function to execute selected command
 function Execute-Command {
-  param (
-    [int]$option
-  )
-    
-  switch ($option) {
-    1 { Start-Process -FilePath "curl.exe" -ArgumentList "-Lo $Env:TEMP\wss\CloudflareWarp.msi", "https://1111-releases.cloudflareclient.com/win/latest"; Start-Process -FilePath "$Env:TEMP\wss\CloudflareWarp.msi" -ArgumentList "/quiet" }
-    2 { Start-Process -FilePath "curl.exe" -ArgumentList "-Lo $Env:TEMP\wss\FirefoxSetup.exe", "https://download.mozilla.org/?product=firefox-latest&os=win64"; Start-Process -FilePath "$Env:TEMP\wss\FirefoxSetup.exe" -ArgumentList "/s" }
-    3 { Start-Process -FilePath "curl.exe" -ArgumentList "-Lo $Env:TEMP\wss\GoogleChrome.msi", "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi"; Start-Process -FilePath "$Env:TEMP\wss\GoogleChrome.msi" -ArgumentList "/quiet" }
-    4 { Start-Process -FilePath "curl.exe" -ArgumentList "-Lo $Env:TEMP\wss\setup.exe", "https://officecdn.microsoft.com/pr/wsus/setup.exe"; Create-ConfigFile; Start-Process -FilePath "$Env:TEMP\wss\setup.exe" -ArgumentList "/configure Configuration.xml" }
-    5 { Start-Process -FilePath "curl.exe" -ArgumentList "-Lo $Env:TEMP\wss\TelegramSetup.exe", "https://telegram.org/dl/desktop/win64"; Start-Process -FilePath "$Env:TEMP\wss\TelegramSetup.exe" -ArgumentList "/verysilent" }
-    6 { Start-Process -FilePath "curl.exe" -ArgumentList "-Lo $Env:TEMP\wss\VSCodeSetup.exe", "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"; Start-Process -FilePath "$Env:TEMP\wss\VSCodeSetup.exe" -ArgumentList "/verysilent /mergetasks=!runcode" }
-    7 { Invoke-RestMethod -Uri https://community.chocolatey.org/install.ps1 | Invoke-Expression }
-    8 { Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression }
-  }
+    param (
+        [int]$option
+    )
+
+    $menuOption = $menuOptions[$option - 1]
+    if ($menuOption.Url) {
+        Write-Host "Downloading $($menuOption.Label)..." -ForegroundColor Yellow
+        Invoke-WebRequest -Uri $menuOption.Url -OutFile "$Env:TEMP\wss\$($menuOption.FileName)"
+        Write-Host "Installing $($menuOption.Label)..." -ForegroundColor Yellow
+        Start-Process -FilePath "$Env:TEMP\wss\$($menuOption.FileName)" -ArgumentList $menuOption.Arguments -Wait
+        Write-Host "Installation of $($menuOption.Label) has been completed.." -ForegroundColor Yellow
+    }
+    elseif ($menuOption.Script) {
+        Write-Host "Installing $($menuOption.Label)..." -ForegroundColor Yellow
+        & $menuOption.Script
+        Write-Host "Installation of $($menuOption.Label) has been completed.." -ForegroundColor Yellow
+    }
 }
+
 
 # Main loop to show menu and process user input
 do {
-  Show-Menu
-  $choice = Read-Host "`nEnter your selection"
-  
-  if ($choice -eq '0') {
-    break
-  }
-    
-  $selectedOptions = Parse-MenuSelection $choice
-  $validOptions = $selectedOptions | Where-Object { $_ -ge 1 -and $_ -le 8 }
-  
-  if ($validOptions.Count -eq 0) {
-    Write-Host "Invalid selection. Please try again." -ForegroundColor Red
-    continue
-  }
-    
-  foreach ($option in $validOptions) {
-    Execute-Command $option
-  }
+    Show-Menu
+    $choice = Read-Host "`nEnter your selection"
+
+    if ($choice -eq '0') {
+        break
+    }
+
+    $selectedOptions = Parse-MenuSelection $choice
+    $validOptions = $selectedOptions | Where-Object { $_ -ge 1 -and $_ -le $menuOptions.Count }
+
+    if ($validOptions.Count -eq 0) {
+        Write-Host "Invalid selection. Please try again." -ForegroundColor Red
+        continue
+    }
+
+    foreach ($option in $validOptions) {
+        Execute-Command $option
+    }
 
 } while ($true)
