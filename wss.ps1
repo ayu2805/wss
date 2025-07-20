92e99b04c74592ec19ceae1931395a1c3a589721
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2025-07-20 15:26:05 +0530

    Update wss.ps1

--- wss.ps1
+++ wss.ps1
@@ -277,6 +277,7 @@ if ($confirmRegistry -match '^(yes|y)$') {
     "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}",
     "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}"
   )
+  New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
   Update-RegistrySettings -settings $registrySettings
   Remove-RegistryKeys -keys $keysToRemove
   # Set-Theme
